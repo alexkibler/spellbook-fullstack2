556464c67186dcdb71e02c436f02436cb029586d
Alex Kibler <Alex Kibler>
2016-02-11 22:31:14 -0500

    kljh

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://alex:spartan117@ds061385.mongolab.com:61385/heroku_8rqzxd8x');
-
+app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
 var spells = require('./spell_routes.js')(app);
 
-var server = app.listen(3000, function () {
-    console.log('Server running at http://127.0.0.1:3000/');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+
+
+
+
+
 
