3a555965047cabdbf2055afe0cee431a05b10fbd
Alex Kibler <Alex Kibler>
2016-02-11 22:23:20 -0500

    First attempt at spellbook

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://alex:spartan117@ds061385.mongolab.com:61385/heroku_8rqzxd8x');
 
-app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use(express.static(__dirname + '/public'));
+var spells = require('./spell_routes.js')(app);
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 
